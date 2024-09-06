9104c3d94e451efb25aed126cc4894cba718346c
cgvillarroel <cgvillarroel@apc.edu.ph>
2024-09-06 21:42:48 +0800

    refactor: modify tester for other modules

--- test.ju.py
+++ test.ju.py
@@ -29,52 +29,38 @@ print(f"\tleft hand : {results[0].shape_results[0]:.5f}")
 print(f"\tright hand: {results[0].shape_results[1]:.5f}")
 
 
-# %% [md]
-# # Location Threshold
-
 # %%
-def testLocation(results: list[Result], threshold: float):
-    true_pos = 0
-    true_neg = 0
-    false_pos = 0
-    false_neg = 0
+def thresholdTesterFactory(false_condition):
 
-    for result in results:
-        if result.gesture1 == result.gesture2:
-            if (result.location_results[0] < threshold
-                    or result.location_results[1] < threshold):
-                false_neg += 1
+    def testThreshold(results: list[Result], threshold: float):
+        true_pos = 0
+        true_neg = 0
+        false_pos = 0
+        false_neg = 0
+
+        for result in results:
+            if (false_condition(result, threshold)):
+                if result.gesture1 == result.gesture2:
+                    false_neg += 1
+                    continue
+
+                true_neg += 1
                 continue
 
-            true_pos += 1
-            continue
+            if result.gesture1 == result.gesture2:
+                true_pos += 1
+                continue
 
-        if (result.location_results[0] < threshold
-                or result.location_results[1] < threshold):
-            true_neg += 1
-            continue
+            false_pos +=1
 
-        false_pos += 1
+        return true_pos, true_neg, false_pos, false_neg
 
-    return true_pos, true_neg, false_pos, false_neg
+    return testThreshold
 
 
-# %%
-# sample test
-true_pos, true_neg, false_pos, false_neg =  testLocation(results, 0)
 
-print(f"True Positives: {true_pos}")
-print(f"True Negatives: {true_neg}")
-print(f"False Positives: {false_pos}")
-print(f"False Negatives: {false_neg}")
-
-accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
-precision = (true_pos) / (true_pos + false_pos)
-recall = (true_pos) / (true_pos + true_neg)
-
-print(f"Accuracy: {accuracy}")
-print(f"Precision: {precision}")
-print(f"Recall: {recall}")
+# %% [md]
+# # Location Threshold
 
 
 # %%
@@ -82,33 +68,46 @@ print(f"Recall: {recall}")
 import matplotlib.pyplot as plt
 import numpy as np
 
-thresholds = [i / 1000 for i in range(825, 1001)]
-accuracies = []
-precisions = []
-recalls = []
+def plotThresholds(test_func, start, end):
+    thresholds = [i / 1000 for i in range(start, end)]
+    accuracies = []
+    precisions = []
+    recalls = []
 
-for threshold in thresholds:
-    print(f"threshold: {threshold}  ", end="\r")
+    for threshold in thresholds:
+        print(f"threshold: {threshold}  ", end="\r")
 
-    true_pos, true_neg, false_pos, false_neg =  testLocation(results, threshold)
-    accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
-    precision = (true_pos) / (true_pos + false_pos)
-    recall = (true_pos) / (true_pos + true_neg)
+        true_pos, true_neg, false_pos, false_neg =  test_func(results, threshold)
+        accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
+        precision = (true_pos) / (true_pos + false_pos)
+        recall = (true_pos) / (true_pos + true_neg)
 
-    accuracies.append(accuracy)
-    precisions.append(precision)
-    recalls.append(recall)
+        accuracies.append(accuracy)
+        precisions.append(precision)
+        recalls.append(recall)
 
 
-np_thresholds = np.array(thresholds)
-np_accuracies = np.array(accuracies)
-np_precisions = np.array(precisions)
-np_recalls = np.array(recalls)
+    np_thresholds = np.array(thresholds)
+    np_accuracies = np.array(accuracies)
+    np_precisions = np.array(precisions)
+    np_recalls = np.array(recalls)
 
-plt.plot(np_thresholds, np_accuracies, color="r", label="accuracy")
-plt.plot(np_thresholds, np_precisions, color="g", label="precision")
-plt.plot(np_thresholds, np_recalls, color="b", label="recall")
+    plt.plot(np_thresholds, np_accuracies, color="r", label="accuracy")
+    plt.plot(np_thresholds, np_precisions, color="g", label="precision")
+    plt.plot(np_thresholds, np_recalls, color="b", label="recall")
 
-plt.title("Threshold vs Stats")
-plt.legend()
-plt.show()
+    plt.title("Threshold vs Stats")
+    plt.legend()
+    plt.show()
+
+
+# %%
+def locationCondition(result, threshold):
+    return result.location_results[0] < threshold or result.location_results[1]< threshold
+
+
+test_location = thresholdTesterFactory(locationCondition)
+
+
+# %%
+plotThresholds(test_location, 825, 1000)
